82791798ce81d96c378103a51209ca22d0ffb879
lucky-panchal <luckykanti31122006@gmail.com>
2025-08-03 19:27:30 +0530

    App.js Updated as required for onboarding requirement

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
+import Onboarding1 from './components/FirstScreen/Onboarding1';
+import Onboarding2 from './components/FirstScreen/Onboarding2';
+import Onboarding3 from './components/FirstScreen/Onboarding3';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator 
+        screenOptions={{ 
+          headerShown: false,
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+          transitionSpec: {
+            open: {
+              animation: 'timing',
+              config: {
+                duration: 300,
+              },
+            },
+            close: {
+              animation: 'timing',
+              config: {
+                duration: 300,
+              },
+            },
+          },
+        }}
+      >
+        <Stack.Screen name="Onboarding1" component={Onboarding1} />
+        <Stack.Screen name="Onboarding2" component={Onboarding2} />
+        <Stack.Screen name="Onboarding3" component={Onboarding3} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
